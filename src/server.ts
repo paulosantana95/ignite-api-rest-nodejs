d5145d9f86682a8c3787c0beb295095a33293cbf
Paulo Santana <paulosantana.sa@gmail.com>
2024-05-30 12:56:47 -0300

    chore: Update database file path and add migrations for transactions table

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/', async () => {
-  const test = await knex('sqlite_schema').select('*')
-  return test
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app.listen({ port: 3333 }).then(() => {
