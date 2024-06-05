1422892efb768c766494d02b74abb31644520065
Paulo Santana <paulosantana.sa@gmail.com>
2024-06-05 01:03:49 -0300

    chore: Update server.ts to log HTTP server port on startup

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP Server Running on http://localhost:3333')
+  console.log(`HTTP Server Running on Port ${env.PORT} 🚀`)
 })
